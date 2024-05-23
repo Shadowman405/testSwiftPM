0cb9a8b56a0e8e228042754566af1ab4fdf50efd
Shadowman405 <maximus405@gmail.com>
2024-05-23 20:55:33 +0300

    delete framework

--- DemoApp/macOS/DemoApp/DemoApp/main.swift
+++
@@ -1,9 +0,0 @@
-import LifoCollections
-
-let someElements = [1, 2, 3, 4, 5]
-let stack = StackStatistics(someElements)
-
-print("Elements in the stack: \(stack.count)")
-if let minimumElement = stack.minimumElement() {
-    print("Minimum element in the stack is: \(minimumElement)")
-}
