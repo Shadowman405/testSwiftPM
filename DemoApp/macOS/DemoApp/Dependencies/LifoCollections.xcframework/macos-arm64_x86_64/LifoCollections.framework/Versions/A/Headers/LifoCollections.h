c08553b8c110f84d901475e2e94fe4dc20312417
Shadowman405 <maximus405@gmail.com>
2024-05-23 20:47:07 +0300

    initial commit

---
+++ DemoApp/macOS/DemoApp/Dependencies/LifoCollections.xcframework/macos-arm64_x86_64/LifoCollections.framework/Versions/A/Headers/LifoCollections.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for LifoCollections.
+FOUNDATION_EXPORT double LifoCollectionsVersionNumber;
+
+//! Project version string for LifoCollections.
+FOUNDATION_EXPORT const unsigned char LifoCollectionsVersionString[];
