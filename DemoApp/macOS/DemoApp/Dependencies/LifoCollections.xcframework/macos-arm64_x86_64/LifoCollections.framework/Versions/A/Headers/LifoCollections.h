bd88b3b349284a6e6082b50f7e0ba3d0a540eea0
Shadowman405 <maximus405@gmail.com>
2024-05-23 20:54:13 +0300

    delete framework

--- DemoApp/macOS/DemoApp/Dependencies/LifoCollections.xcframework/macos-arm64_x86_64/LifoCollections.framework/Versions/A/Headers/LifoCollections.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-//! Project version number for LifoCollections.
-FOUNDATION_EXPORT double LifoCollectionsVersionNumber;
-
-//! Project version string for LifoCollections.
-FOUNDATION_EXPORT const unsigned char LifoCollectionsVersionString[];
